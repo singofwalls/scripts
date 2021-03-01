f7ca713e9baaae9822821938d208e969bbca94b8
Reece Mathews <mathewsreece@gmail.com>
2021-03-01 12:20:28 -0600

    Append pdfs in order of file name (assumes file names are numbers)

--- pdf.py
+++ pdf.py
@@ -1,18 +1,22 @@
 import os
 import PyPDF2
 import img2pdf
+from pathlib import Path
 
 pdfWriter = PyPDF2.PdfFileWriter()
 
 # Create pdfs from images
 for filename in os.listdir():
-    if filename.endswith(".jpg"):
+    if filename.endswith(".png"):
         with open(f"image_pages/{filename}.pdf", "wb") as f:
             f.write(img2pdf.convert([filename]))
 
 # concat all pdfs
 pdfs = []
-for filename in os.listdir() + ['image_pages/' + f for f in os.listdir('image_pages')]:
+files = os.listdir() + ['image_pages/' + f for f in os.listdir('image_pages')]
+files = [f for f in files if os.path.splitext(f)[1] == ".pdf"]
+files = sorted(files, key=lambda f: int(Path(f).name.split(".")[0]))
+for filename in files:
     if filename.endswith(".pdf"):
         pdf = open(filename, "rb")
         pdfs.append(pdf)
