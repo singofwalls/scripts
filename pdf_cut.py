e04a74b9e14027f6f6b03b172cf5ba895ae18647
singofwalls <mathewsreece@gmail.com>
2022-05-10 17:20:06 -0500

    ranges

--- pdf_cut.py
+++ pdf_cut.py
@@ -2,7 +2,13 @@ from PyPDF2 import PdfFileWriter, PdfFileReader
 
 inputpdf = PdfFileReader(open("input.pdf", "rb"))
 
+pages = "5-20, 22-37, 39-46, 48-55, 57-64, 66-71, 73-79"
+pages = [range(*[int(l) for l in r.split("-")]) for r in pages.split(", ")]
+
 output = PdfFileWriter()
-output.addPage(inputpdf.getPage(1))
+for r in pages:
+    for p in r:
+        output.addPage(inputpdf.getPage(p))
+
 with open("output.pdf", "wb") as outputStream:
     output.write(outputStream)
